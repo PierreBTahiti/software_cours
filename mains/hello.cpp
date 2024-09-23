efd4f097e785bcfb5a8687b085e37aca46241dd6
PierreBTahiti <PierreBTahiti@users.noreply.github.com>
2024-09-23 14:50:25 +0000

    Apply format changes

--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,38 +1,35 @@
 #include <HelloProjConfig.h>
 #include <iostream>
 <<<<<<< HEAD
-//j'ai fait les nv cmake config
+    // j'ai fait les nv cmake config
 
-
-void hello_world() {
-	
-	
-	std::cout << "Hello World!\n";
-}
-void print_version() {
-	std::cout << "project version is "
-		<< HelloProj_VERSION_MAJOR
-		<< "."
-		
-		<< HelloProj_VERSION_MINOR
-		<< std::endl;
-=======
-
-// j'ai fait les nv cmake config
-
-void hello_world()
+    void
+    hello_world()
 {
     std::cout << "Hello World!\n";
 }
-
 void print_version()
 {
     std::cout << "project version is " << HelloProj_VERSION_MAJOR << "."
+
               << HelloProj_VERSION_MINOR << std::endl;
+=======
+
+    // j'ai fait les nv cmake config
+
+    void hello_world()
+    {
+        std::cout << "Hello World!\n";
+    }
+
+    void print_version()
+    {
+        std::cout << "project version is " << HelloProj_VERSION_MAJOR << "."
+                  << HelloProj_VERSION_MINOR << std::endl;
 >>>>>>> ff8312810643151690449bd8608e76ba0597026f
-}
-int main()
-{
-    hello_world();
-    print_version();
-}
+    }
+    int main()
+    {
+        hello_world();
+        print_version();
+    }
