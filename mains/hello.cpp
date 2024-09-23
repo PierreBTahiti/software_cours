6ec86d7fdd4e3905627c7c944563e6250873821d
boutin15u <boutin15u@BI-FDE-F-W125>
2024-09-23 16:48:19 +0200

    fix(format): style pour tester

--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,18 +1,18 @@
 #include <HelloProjConfig.h>
 #include <iostream>
-
-
 //j'ai fait les nv cmake config
 
 
 void hello_world() {
+	
+	
 	std::cout << "Hello World!\n";
 }
-
 void print_version() {
 	std::cout << "project version is "
 		<< HelloProj_VERSION_MAJOR
 		<< "."
+		
 		<< HelloProj_VERSION_MINOR
 		<< std::endl;
 }
